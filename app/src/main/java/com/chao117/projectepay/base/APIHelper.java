7c91268e04c37bd2938f6071e79c55be2f1d4e54
chao <chen404i@outlook.com>
2017-04-21 11:54:04 +0800

    APIHelper v.1.0
    APIHelper v.1.0 : 确定了api 2.0 列表,对失效的 api 1.0 常量作了标记,项目全面弃用 api
    1.0,服务器保留 api 1.0.

--- app/src/main/java/com/chao117/projectepay/base/APIHelper.java
+++ app/src/main/java/com/chao117/projectepay/base/APIHelper.java
@@ -10,37 +10,25 @@ import com.chao117.projectepay.base.constant.DatabaseField;
 public interface APIHelper extends DatabaseField {
     //服务器通信用标记
     String JSON = "json";
-    String REQUEST_CODE = "requestCode";
-
-    //返回 json 串必须有的类型
-    String KIND = "kind";
-    String ERROR_CODE = "errorCode";
-    String RESULT = "result";
-
-
-    String FIELD_WHAT = "what";
-
-    String OBJECT = "object";
-    String OBJECT_ARRY = "objectArray";
-
-    //kind
-    String KIND_OBJ = "kindObj";
-    String KIND_ARR = "kindArr";
 
     //result
     String RESULT_SUCCESS = "resultSuccess";
     String RESULT_FAILURE = "resultFailure";
 
-    //请求的 api 功能
+    /**
+     * api 1.0
+     */
     String API_TYPE_TEST = "test";
     String API_TYPE_LOGIN_BAK = "loginbak";
+    String API_TYPE_CHECK_USER = "forUser";
+
+    /**
+     * api 2.0
+     */
     //api 2.0:使用 ServerResult 返回结果
     String API_TYPE_LOGIN = "login";
     String API_TYPE_REGISTER = "register";
     String API_TYPE_PUBLISH = "publish";
-
-    String API_TYPE_CHECK_USER = "forUser";
-
     //2017年04月21日10:55:46
     String API_TYPE_BROWSE = "simpleBrowse";                //简单浏览
     String API_TYPE_PASSWORD = "password";                  //修改密码
@@ -51,9 +39,32 @@ public interface APIHelper extends DatabaseField {
     String API_TYPE_CHECK_TRANSACTION = "transCheck";       //查看交易状态
     String API_TYPE_UPDATE_TRANSACTION = "transUpdate";     //更新交易状态
 
-    String API_TYPE_
+    String API_TYPE_ADD_SHIP_ADDRESS = "addShipAddress";    //新增收货地址
+    String API_TYPE_REQ_SHIP_ADDRESS = "requestShipAddress";//请求收货地址
+    String API_TYPE_ADD_HISTORY = "addHistory";
+    String API_TYPE_REQ_HISTORY = "requestHistory";
+
+    String API_TYPE_CHECK_DB_VERSION = "checkDatabaseVer";  //检查本地服务器数据库版本号,先不实现
+    String API_TYPE_UPDATE_LOCAL_DB = "updateLocalDatabase";//更新本地数据库
 
     String API_TYPE_SEARCH_GOODS = "searchGoods";           //搜索商品
     String API_TYPE_REQUEST_MSGS = "requestMessages";       //请求 message 信息
     String API_TYPE_PUBLISH_MSG = "publishMessage";         //发布留言
+
+
+    /**
+     * 以下为已经失效的常量
+     * api 1.0 阶段的常量
+     */
+
+    String REQUEST_CODE = "requestCode";
+    //返回 json 串必须有的类型
+    String KIND = "kind";
+    String ERROR_CODE = "errorCode";
+    String RESULT = "result";
+    String FIELD_WHAT = "what";
+    String OBJECT = "object";
+    String OBJECT_ARRY = "objectArray";
+    String KIND_OBJ = "kindObj";
+    String KIND_ARR = "kindArr";
 }
