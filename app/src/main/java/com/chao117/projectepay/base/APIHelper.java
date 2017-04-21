47bec24234c6950079441d5eecc31658113b4e80
chao <chen404i@outlook.com>
2017-04-21 11:28:47 +0800

    APIHelper update

--- app/src/main/java/com/chao117/projectepay/base/APIHelper.java
+++ app/src/main/java/com/chao117/projectepay/base/APIHelper.java
@@ -33,10 +33,27 @@ public interface APIHelper extends DatabaseField {
 
     //请求的 api 功能
     String API_TYPE_TEST = "test";
-    String API_TYPE_LOGIN_BAK ="loginbak";
+    String API_TYPE_LOGIN_BAK = "loginbak";
     //api 2.0:使用 ServerResult 返回结果
     String API_TYPE_LOGIN = "login";
     String API_TYPE_REGISTER = "register";
     String API_TYPE_PUBLISH = "publish";
+
     String API_TYPE_CHECK_USER = "forUser";
+
+    //2017年04月21日10:55:46
+    String API_TYPE_BROWSE = "simpleBrowse";                //简单浏览
+    String API_TYPE_PASSWORD = "password";                  //修改密码
+    String API_TYPE_FAV = "fav";                            //收藏商品
+    String API_TYPE_REQUEST_TRANSACTION = "transRequest";   //请求交易
+    String API_TYPE_ACCEPT_TRANSACTION = "transAccept";     //接受交易
+    String API_TYPE_CANCEL_TRANSACTION = "transCancel";     //取消交易
+    String API_TYPE_CHECK_TRANSACTION = "transCheck";       //查看交易状态
+    String API_TYPE_UPDATE_TRANSACTION = "transUpdate";     //更新交易状态
+
+    String API_TYPE_
+
+    String API_TYPE_SEARCH_GOODS = "searchGoods";           //搜索商品
+    String API_TYPE_REQUEST_MSGS = "requestMessages";       //请求 message 信息
+    String API_TYPE_PUBLISH_MSG = "publishMessage";         //发布留言
 }
