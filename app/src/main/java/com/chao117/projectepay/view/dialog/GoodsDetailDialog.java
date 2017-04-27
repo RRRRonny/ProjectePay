18b6d7d481932d029ae9e0953b6ec1076b6f8760
chao <chen404i@outlook.com>
2017-04-27 18:40:46 +0800

    common update
    1.收藏按钮无法正常运作

--- app/src/main/java/com/chao117/projectepay/view/dialog/GoodsDetailDialog.java
+++ app/src/main/java/com/chao117/projectepay/view/dialog/GoodsDetailDialog.java
@@ -2,6 +2,8 @@ package com.chao117.projectepay.view.dialog;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -30,16 +32,46 @@ public class GoodsDetailDialog {
     private TextView tvDiaGoodsNameDetail;
     private TextView tvDiaGoodsPriceDetail;
     private TextView tvDiaGoodsLocationDetail;
+    private TextView tvDiaGoodFav;
+    private boolean isFav;
+    private int favColor;
+    private int unFavColor;
+    private Drawable favDrawable;
+    private Drawable unFavDrawable;
 
     public GoodsDetailDialog(Context context, Goods goods) {
         this.context = context;
         this.goods = goods;
+
+        favColor = context.getResources().getColor(R.color.colorFav, null);
+        unFavColor = context.getResources().getColor(R.color.colorDivider, null);
+        favDrawable = context.getResources().getDrawable(R.mipmap.fav_, null);
+        favDrawable.setBounds(0, 0, 32, 32);
+        unFavDrawable = context.getResources().getDrawable(R.mipmap.fav, null);
+        unFavDrawable.setBounds(0, 0, 32, 32);
+
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         rootView = inflater.inflate(R.layout.dialog_goods_details, null);
         tvDiaGoodsBrandDetail = (TextView) rootView.findViewById(R.id.tvDiaGoodsBrandDetail);
         tvDiaGoodsNameDetail = (TextView) rootView.findViewById(R.id.tvDiaGoodsNameDetail);
         tvDiaGoodsPriceDetail = (TextView) rootView.findViewById(R.id.tvDiaGoodsPriceDetail);
         tvDiaGoodsLocationDetail = (TextView) rootView.findViewById(R.id.tvDiaGoodsLocationDetail);
+        tvDiaGoodFav = (TextView) rootView.findViewById(R.id.tvDiaGoodFav);
+        tvDiaGoodFav.setCompoundDrawables(unFavDrawable, null, null, null);
+        isFav = false;
+        tvDiaGoodFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFav) {
+                    Log.v("tag","UN_FAV!!!");
+                    tvDiaGoodFav.setCompoundDrawables(unFavDrawable, null, null, null);
+                } else {
+                    Log.v("tag","FAV!!!");
+                    tvDiaGoodFav.setCompoundDrawables(favDrawable, null, null, null);
+                }
+                isFav = !isFav;
+            }
+        });
     }
 
     public void show() {
@@ -49,7 +81,6 @@ public class GoodsDetailDialog {
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
                     }
                 })
                 .create();
